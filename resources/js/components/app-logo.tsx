7207fbbc4dffd0d3b59ca00791dfdaa30cbac009
Giovanni Pellerano <pelleranogiovanni@hotmail.com>
2026-05-05 23:21:21 -0300

    se actualiza logo

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -1,14 +1,9 @@
-import AppLogoIcon from './app-logo-icon';
-
 export default function AppLogo() {
     return (
-        <>
-            <div className="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground">
-                <AppLogoIcon className="size-5 fill-current text-white dark:text-black" />
-            </div>
-            <div className="ml-1 grid flex-1 text-left text-sm">
-                <span className="mb-0.5 truncate leading-tight font-semibold">EPOS</span>
-            </div>
-        </>
+        <img
+            src="/logo.png"
+            alt="EPOS"
+            className="h-10 w-auto object-contain max-w-[160px] group-data-[collapsible=icon]:hidden"
+        />
     );
 }
