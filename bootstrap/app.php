d2844474721958a27841fd15bfa267fb78ae12ce
root <root@srv1329370.hstgr.cloud>
2026-05-11 19:06:36 +0000

    fix: resolve double click issue on login

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -27,6 +28,8 @@ return Application::configure(basePath: dirname(__DIR__))
             'api/telegram/webhook/*',
             'logout', // Logout es idempotente, no necesita CSRF
         ]);
+
+        $middleware->trustProxies(at: '*', headers: Request::HEADER_X_FORWARDED_FOR | Request::HEADER_X_FORWARDED_HOST | Request::HEADER_X_FORWARDED_PROTO | Request::HEADER_X_FORWARDED_PORT);
 
         $middleware->web(append: [
             HandleAppearance::class,
